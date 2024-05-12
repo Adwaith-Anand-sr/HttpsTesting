40c6b262a939f04acc27fcd264ca1e95d2cdb73e
Adwaith-anand-sr <sreeadwa@gmail.com>
2024-05-12 08:14:46 +0530

    aaded files

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const https = require('https');
+const fs = require('fs');
+const path = require("path");
+
+const app = express();
+
+app.get('/', (req, res) => {
+   console.log("router get started..")
+  res.send('Hello from HTTPS server!');
+});
+
+const server = https.createServer({
+  key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+  cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
+}, app);
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
